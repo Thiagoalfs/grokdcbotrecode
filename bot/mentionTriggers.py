51456c10168f5063000736807fe4f4ac52dfa61b
Thiago Alonso <thiago.alfs@gmail.com>
2026-05-12 19:31:00 -0300

    Update mentionTriggers.py

--- bot/mentionTriggers.py
+++ bot/mentionTriggers.py
@@ -1,18 +1,10 @@
 import re, discord
 from generalFunctions import respostas, ytdlp
 
-try:
-    from hiddentriggers.hiddenMentionTriggers import hidden_mention_triggers
-except (ImportError, ModuleNotFoundError):
-    hidden_mention_triggers = None
-
 async def mention_triggers(bot, message):
     if bot.user in message.mentions:
 
-        if hidden_mention_triggers and await hidden_mention_triggers(bot, message):
-            return True
-    
-        elif message.content in (f"<@{bot.user.id}>", f"<@!{bot.user.id}>"):
+        if message.content in (f"<@{bot.user.id}>", f"<@!{bot.user.id}>"):
             await message.channel.send("oi fi po fala")
             return True
         
